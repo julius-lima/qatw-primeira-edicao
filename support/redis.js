1f73851b5474dd19f379668bdbce040552181d18
Julio Lima <juliobrasilg12@gmail.com>
2025-08-22 10:44:35 -0300

    Ajuste na identação do redis.js

--- support/redis.js
+++ support/redis.js
@@ -10,11 +10,11 @@ const queueName = 'twoFactorQueue';
 const queue = new Queue(queueName, { connection });
 
 export const getJob = async () => {
-    const jobs = await queue.getJobs() // busca todos os jobs na fila
-    console.log(jobs[0].data.code) // exibe o primeiro job no console
-    return jobs[0].data.code;  // retorna o primeiro job
+  const jobs = await queue.getJobs() // busca todos os jobs na fila
+  console.log(jobs[0].data.code) // exibe o primeiro job no console
+  return jobs[0].data.code;  // retorna o primeiro job
 }
 
 export const cleanJobs = async () => {
-    await queue.obliterate({force:true}) // limpa todos os jobs na fila
+  await queue.obliterate({ force: true }) // limpa todos os jobs na fila
 }
